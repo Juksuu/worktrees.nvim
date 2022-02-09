bfe4a796ffc6d0a50903f1b0e351c6d78712d48d
Frans Paasonen <franspaasonen@gmail.com>
2022-02-09 20:01:44 +0200

    fix(commands): Modify command argument parsing

--- lua/worktrees/init.lua
+++ lua/worktrees/init.lua
@@ -11,39 +11,37 @@ M.setup = function(opts)
 
     vim.notify_once("Worktree plugin initialized")
 
-    vim.api.nvim_add_user_command(
-        "GitWorktreeCreate",
-        M.new_worktree_command,
-        { nargs = "*" }
-    )
-    vim.api.nvim_add_user_command(
-        "GitWorktreeSwitch",
-        M.switch_worktree_command,
-        { nargs = 1 }
-    )
+    vim.api.nvim_add_user_command("GitWorktreeCreate", function(input)
+        local args = input.args:split_string(" ")
+        if not args[1] then
+            return
+        end
+
+        local create_opts = {
+            branch = args[1],
+            folder = args[2],
+            base_branch = args[3],
+        }
+
+        M.new_worktree(create_opts)
+    end, { nargs = "*" })
+
+    vim.api.nvim_add_user_command("GitWorktreeSwitch", function(input)
+        local args = input.args:split_string(" ")
+
+        if not args[1] then
+            return
+        end
+
+        M.switch_worktree(args[1])
+    end, { nargs = 1 })
     vim.api.nvim_add_user_command("GitWorktreeTrack", M.new_worktree_track, {})
-end
-
-M.new_worktree_command = function(input)
-    local args = input.args:split_string(" ")
-
-    if not args[1] then
-        return
-    end
-
-    local create_opts = { branch = args[1] }
-    if args[2] then
-        create_opts["folder"] = args[2]
-    end
-    if args[3] then
-        create_opts["base_branch"] = args[3]
-    end
-
-    M.new_worktree(create_opts)
 end
 
 M.new_worktree = function(opts)
     vim.notify("Creating new worktree")
+
+    vim.notify(vim.inspect(opts))
 
     if not opts.branch then
         return
@@ -70,16 +68,6 @@ M.new_worktree = function(opts)
     create_job:sync()
 end
 
-M.switch_worktree_command = function(input)
-    local args = input.args:split_string(" ")
-
-    if not args[1] then
-        return
-    end
-
-    M.switch_worktree(args[1])
-end
-
 M.switch_worktree = function(input)
     vim.notify("Switching to another worktree")
 
@@ -88,7 +76,7 @@ M.switch_worktree = function(input)
 
     local worktrees = utils.get_worktrees()
     local path = nil
-    for _, worktree in pairs(worktrees) do
+    for _, worktree in ipairs(worktrees) do
         if worktree.folder == input or worktree.branch == input then
             path = worktree.path
             break
